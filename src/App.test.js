696af340287668aa91d50908a4878336d8648738
Jacinto Mba Cantero <jacmba@gmail.com>
2022-02-15 17:24:19 +0000

    Fix initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App title="test cards app" />);
+  const titleElement = screen.getByText(/test cards app/i);
+  expect(titleElement).toBeInTheDocument();
 });
